395df27bde124e5ef22735b8695474bb11f1160c
Fokstal <klabu2016@mail.ru>
2023-03-31 22:37:11 +0300

    Добавьте файлы проекта.

---
+++ UniverseOfTanks/Form1.cs
@@ -0,0 +1,14 @@
+namespace UniverseOfTanks
+{
+	public partial class Form1 : Form
+	{
+		private Graphics? _graphics;
+		private int _xHeatBoxTank;
+		private int _yHeatBoxTank;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
